00810f54c8e36aaf757b4a7b1604d14bcc10bcb0
0xCoto <cotothearcher@gmail.com>
2019-12-12 20:04:21 +0200

    Update plot_hi.py

--- plot_hi.py
+++ plot_hi.py
@@ -12,7 +12,6 @@ parser.add_argument('freq')
 parser.add_argument('samp_rate')
 parser.add_argument('nchan')
 parser.add_argument('nbin')
-parser.add_argument('nbin_off')
 args = parser.parse_args()
 
 def decibel(x):
@@ -52,7 +51,6 @@ exec(args.freq)
 exec(args.samp_rate)
 exec(args.nchan)
 exec(args.nbin)
-exec(args.nbin_off)
 obs_on = "observation.dat"
 obs_off = "off"+str(nchan)+".dat"
 
@@ -66,7 +64,7 @@ z = z*10000
 zmean = np.nanmean(z,axis=0)
 
 #Load OFF data
-z_off = np.fromfile(obs_off, dtype="float32").reshape(-1, nchan)/nbin_off
+z_off = np.fromfile(obs_off, dtype="float32").reshape(-1, nchan)/nbin
 z_off = z_off*10000
 z_offmean = np.nanmean(z_off,axis=0)
 
