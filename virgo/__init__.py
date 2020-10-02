632dd5ce64b47c05620757d031440fd4c5d433fa
0xCoto <cotothearcher@gmail.com>
2020-10-02 15:02:24 +0300

    Update power distribution plot bins

--- virgo/__init__.py
+++ virgo/__init__.py
@@ -344,12 +344,12 @@ def plot(obs_parameters='', n=0, m=0, f_rest=0, dB=False, obs_file='observation.
 
 	ax5 = fig.add_subplot(gs[1, 1])
 
-	ax5.hist(power, int(np.size(power)/50), density=1, alpha=0.5, color='royalblue', orientation='horizontal', zorder=10)
+	ax5.hist(power, np.max([int(np.size(power)/50),10], density=1, alpha=0.5, color='royalblue', orientation='horizontal', zorder=10)
 	ax5.set_xlim()
 	ax5.set_ylim()
 	ax5.plot(best_fit(power)[1], best_fit(power)[0], '--', color='blue', label='Best fit (Raw)', zorder=20)
 	if m != 0:
-		ax5.hist(power_clean, int(np.size(power_clean)/50), density=1, alpha=0.5, color='orangered', orientation='horizontal', zorder=10)
+		ax5.hist(power_clean, np.max([int(np.size(power)/50),10], density=1, alpha=0.5, color='orangered', orientation='horizontal', zorder=10)
 		ax5.plot(best_fit(power_clean)[1], best_fit(power)[0], '--', color='red', label='Best fit (Median)', zorder=20)
 	ax5.get_shared_x_axes().join(ax5, ax4)
 	ax5.set_yticklabels([])
