fd1fb50d2519994674eb1bc7815f13b2fe6c679f
0xCoto <cotothearcher@gmail.com>
2021-01-14 08:43:11 +0200

    Add new functions

--- virgo/__init__.py
+++ virgo/__init__.py
@@ -1,1 +1,1 @@
-from .virgo import simulate, predict, equatorial, galactic, map_hi, observe, plot, plot_rfi, monitor_rfi
+from .virgo import simulate, predict, equatorial, galactic, frequency, wavelength, gain, A_e, beamwidth, NF, T_noise, G_T, SEFD, snr, map_hi, observe, plot, plot_rfi, monitor_rfi
