acbfb1fbdd6b304cd3faeede128c3318a7b456b9
0xCoto <cotothearcher@gmail.com>
2021-04-02 03:49:23 +0300

    Update to v3.6.6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Apostolos Spanakis-Misirlis'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.6.5'
+release = '3.6.6'
 
 
 # -- General configuration ---------------------------------------------------
