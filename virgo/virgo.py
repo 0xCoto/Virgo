c178828fea2ba2ba8c2f6eb49af467f205e25413
Kevin A. Wilson <kwilson@avalere.com>
2021-07-18 01:41:26 -0400

    added meta headers to plot

--- virgo/virgo.py
+++ virgo/virgo.py
@@ -500,7 +500,7 @@ def observe(obs_parameters, spectrometer='wola', obs_file='observation.dat', sta
 			channels: int: Number of frequency channels (FFT size)
 			t_sample: float: Integration time per FFT sample
 			duration: float: Total observing duration [sec]
-			loc: string: longitude, latitude, and elevation of observation (float, separated by spaces)
+			loc: string: latitude, longitude, and elevation of observation (float, separated by spaces)
 			ra_dec: string: right ascension and declination of observation target (float, separated by space)
 			az_alt: string: azimuth and altitude of observation target (float, separated by space; takes precedence over ra_dec)
 		spectrometer: string. Spectrometer flowchart/pipeline ('WOLA'/'FTF')
@@ -576,7 +576,7 @@ loc='''+str(loc)+'''
 ra_dec='''+str(ra_dec)+'''
 az_alt='''+str(az_alt))
 
-def plot(obs_parameters='', n=0, m=0, f_rest=0, slope_correction=False, dB=False, vlsr=False, cal_ylim=[0,0], rfi=[], xlim=[0,0], ylim=[0,0], dm=0,
+def plot(obs_parameters='', n=0, m=0, f_rest=0, slope_correction=False, dB=False, vlsr=False, meta=False, avg_ylim=[0,0], cal_ylim=[0,0], rfi=[], xlim=[0,0], ylim=[0,0], dm=0,
 	 obs_file='observation.dat', cal_file='', waterfall_fits='', spectra_csv='', power_csv='', plot_file='plot.png'):
 	'''
 	Process, analyze and plot data.
@@ -601,7 +601,9 @@ def plot(obs_parameters='', n=0, m=0, f_rest=0, slope_correction=False, dB=False
 		slope_correction: bool. Correct slope in poorly-calibrated spectra using linear regression
 		dB: bool. Display data in decibel scaling
 		vlsr: bool. Display graph in VLSR frame of reference
-		rfi: list of tuples. Blank frequency channels contaminated with RFI ([(low_frequency, high_frequency), ...]) [Hz]
+		meta: bool. Display header with date, time, and target
+		rfi: list. Blank frequency channels contaminated with RFI ([low_frequency, high_frequency]) [Hz]
+		avg_ylim: list. Averaged plot y-axis limits ([low, high])
 		cal_ylim: list. Calibrated plot y-axis limits ([low, high])
 		xlim: list. x-axis limits ([low_frequency, high_frequency]) [Hz]
 		ylim: list. y-axis limits ([start_time, end_time]) [Hz]
@@ -891,6 +893,14 @@ def plot(obs_parameters='', n=0, m=0, f_rest=0, slope_correction=False, dB=False
 		fig = plt.figure(figsize=(21, 15))
 		gs = GridSpec(2, 2)
 
+	if meta:
+		from astropy.coordinates import get_constellation
+
+		epoch = (mjd - 40587) * 86400.0
+		meta_title = 'Date and Time: ' + time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime(epoch)) + '       '
+		meta_title += 'Target: ' + obs_coord.to_string('hmsdms', precision=0) + ' in ' + get_constellation(obs_coord) + '\n'
+		plt.suptitle(meta_title, fontsize=18)
+
 	# Plot Average Spectrum
 	ax1 = fig.add_subplot(gs[0, 0])
 	ax1.plot(frequency, avg_spectrum)
@@ -900,14 +910,22 @@ def plot(obs_parameters='', n=0, m=0, f_rest=0, slope_correction=False, dB=False
 		ax1.set_xlim(xlim[0], xlim[1])
 	ax1.ticklabel_format(useOffset=False)
 	ax1.set_xlabel('Frequency (MHz)')
+	if avg_ylim != [0,0]:
+		ax1.set_ylim(avg_ylim[0], avg_ylim[1])
 	if dB:
 		ax1.set_ylabel('Relative Power (dB)')
 	else:
 		ax1.set_ylabel('Relative Power')
-	if f_rest != 0:
-		ax1.set_title('Average Spectrum\n')
+
+	if vlsr:
+		cal_title = r'$Average\ Spectrum\ (V_{LSR})$'
 	else:
-		ax1.set_title('Average Spectrum')
+		cal_title = 'Average Spectrum'
+
+	if f_rest != 0:
+		cal_title += '\n'
+
+	ax1.set_title(cal_title)
 	ax1.grid()
 
 	if xlim == [0,0] and f_rest != 0:
@@ -941,7 +959,7 @@ def plot(obs_parameters='', n=0, m=0, f_rest=0, slope_correction=False, dB=False
 		ax2.set_ylabel('Signal-to-Noise Ratio (S/N)')
 
 		if vlsr:
-			cal_title = r'$Calibrated Spectrum (V_{LSR})$' + '\n'
+			cal_title = r'$Calibrated\ Spectrum\ (V_{LSR})$' + '\n'
 		else:
 			cal_title = 'Calibrated Spectrum\n'
 
@@ -1066,6 +1084,9 @@ def plot_rfi(rfi_parameters, data='rfi_data', dB=True, plot_file='plot.png'):
 	channels = rfi_parameters['channels']
 	t_sample = rfi_parameters['t_sample']
 	duration = rfi_parameters['duration']
+	loc = rfi_parameters['loc']
+	ra_dec = rfi_parameters['ra_dec']
+	az_alt = rfi_parameters['az_alt']
 
 	def decibel(x):
 		if dB: return 10.0*np.log10(x)
@@ -1152,6 +1173,9 @@ def monitor_rfi(f_lo, f_hi, obs_parameters, data='rfi_data'):
 			channels: int: Number of frequency channels (FFT size)
 			t_sample: float: Integration time per FFT sample
 			duration: float: Total observing duration [sec]
+			loc: string: latitude, longitude, and elevation of observation (float, separated by spaces)
+			ra_dec: string: right ascension and declination of observation target (float, separated by space)
+			az_alt: string: azimuth and altitude of observation target (float, separated by space; takes precedence over ra_dec)
 		data: string. Survey data directory to output individual observations to
 	'''
 	dev_args = obs_parameters['dev_args']
@@ -1161,6 +1185,9 @@ def monitor_rfi(f_lo, f_hi, obs_parameters, data='rfi_data'):
 	bandwidth = obs_parameters['bandwidth']
 	channels = obs_parameters['channels']
 	duration = obs_parameters['duration']
+	loc = obs_parameters['loc']
+	ra_dec = obs_parameters['ra_dec']
+	az_alt = obs_parameters['az_alt']
 
 	t_sample = 0.1
 
@@ -1183,7 +1210,11 @@ def monitor_rfi(f_lo, f_hi, obs_parameters, data='rfi_data'):
 			'channels': channels,
 			't_sample': t_sample,
 			'duration': duration,
-			'f_lo': f_lo
+			'f_lo': f_lo,
+			'loc': loc,
+			'ra_dec': ra_dec,
+			'az_alt': az_alt
+
 		}
 
 		# Run RFI monitor
